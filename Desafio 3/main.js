d22fd98d69ec53b26dc52880f9b212b518e3adef
fernandacaperon <fernandacaperon@gmail.con>
2021-09-27 18:16:54 -0300

    desafio 3

---
+++ Desafio 3/main.js
@@ -0,0 +1,8 @@
+// Pedir un número por prompt, repetir la salida del mensaje la cantidad de veces ingresada.
+
+
+let numero = prompt ("Ingresa un numero")
+
+for (0; numero <= 10 ; numero++) {
+    console.log ("Bienvenido")
+}

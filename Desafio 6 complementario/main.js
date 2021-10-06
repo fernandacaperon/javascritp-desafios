1e7368fb40fa2a264a75aab85d54e77210bec1f7
fernandacaperon <fernandacaperon@gmail.con>
2021-10-06 20:07:57 -0300

    desafio complementario

---
+++ Desafio 6 complementario/main.js
@@ -0,0 +1,11 @@
+// catalogo 
+
+const sans = ["Open Sans", "Roboto", "Noto", "Raleway", "Monserrat", "Ubuntu"];
+const serif  = ["Alegreya", "Cinzel", "Playfair Display", "Pt Serif", "Libre Baskerville", "Bree Serif" ];
+const catalogo = sans.concat(serif);
+const orden = catalogo.sort();
+
+console.log (catalogo)
+console.log (orden)
+
+

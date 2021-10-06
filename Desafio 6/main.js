495bf69302d5636ca1d86f231960a61c46fe9b16
fernandacaperon <fernandacaperon@gmail.con>
2021-10-06 18:38:36 -0300

    desafio 6

---
+++ Desafio 6/main.js
@@ -0,0 +1,7 @@
+// catalogo 
+
+const sans = ["Open Sans", "Roboto", "Noto", "Raleway", "Monserrat", "Ubuntu"];
+const serif  = ["Alegreya", "Cinzel", "Playfair Display", "Pt Serif", "Libre Baskerville", "Bree Serif" ];
+const catalogo = sans.concat(serif);
+
+console.log (catalogo)

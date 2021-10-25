a50964e3e2813ebe5cb3f5c3ca34ca893628b470
fernandacaperon <fernandacaperon@gmail.con>
2021-10-25 08:57:16 -0300

    cambios

--- Proyecto final/main.js
+++ Proyecto final/main.js
@@ -1,23 +1,12 @@
-function Tester (nombre,estilo,peso,autor) {
-    this.nombre = nombre;
-    this.estilo = estilo;
-    this.peso = peso
-    this.autor = autor
+const parrafo = document.getElementById ("parrafo")
+
+const boton = document.getElementById ("btn")
+
+const escribir = () => { 
+    let texto = alert ("El texto que probaste...")
+    parrafo.textContent = texto
 }
 
-let nombre = prompt ("Que tipografia queres probar?")
-let estilo = prompt ("Que estilo?")
-let peso = prompt ("Que peso?")
-let autor = prompt ("Que autor?")
-
-
-const tester1 = new Tester (nombre, estilo,peso,autor);
-
-console.log (`Probaste ${tester1}`)
-
-// // catalogo 
-
-// const sans = ["Open Sans", "Roboto", "Noto", "Raleway", "Monserrat", "Ubuntu"];
-// const serif  = ["Alegreya", "Cinzel", "Playfair Display", "Pt Serif", "Libre Baskerville", "Bree Serif" ];
-// const catalogo = sans.concat(serif);
-
+boton.addEventListener ("click", () => {
+    escribir ()
+}) 
